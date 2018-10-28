92dc13f8a72bb4bdcd54b926b04705b0d27f04d8
Daedalusz <daedalus.z@gmail.com>
2018-10-28 15:40:36 +0900

    Partial implementation of basic shape scaling with sprites.

--- GameObjects.py
+++ GameObjects.py
@@ -4,29 +4,60 @@ import pymunk
 # Any Physical Object in the Game
 class GameObject(pyglet.sprite.Sprite):
 
-    def __init__(self, space=pymunk.Space ,*args, **kwargs):
+    def __init__(self,space=pymunk.space,mass=10, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # pymunk Physics
         self.body = pymunk.Body(moment=0,mass=0)
         self.body.position = (100, 100)
-        self.shape = pymunk.shapes.Circle(body=self.body, radius=100)
-        self.shape.mass = 100
+        self.space = space
+        self.update_shape()
+        self.mass = mass
 
         # TODO - Add a default shape?
 
-
-    # Update state of this item. By default we just move our position based on our velocity
+    # Update x/y for sprite drawing. NOTE: This is part of the rendering loop
     def update(self, window, dt):
         # Update Sprite Position based on physics body location.
-        print(self.body.position)
-        self.x = self.body.position.x
-        self.y = self.body.position.y
 
+        self.x = self.body.position.x - self.offsetx
+        self.y = self.body.position.y - self.offsety
+        print(self.body.mass)
 
-    def add_to_space(self, space):
-        space.add(self.body)
-        space.add(self.shape)
+    def add_to_space(self):
+        if self.body not in self.space.bodies:
+            self.space.add(self.body)
+
+        if self.shape not in self.space.bodies:
+            self.space.add(self.shape)
+
+    # Update the size of our shape and relative location of the sprite to render
+    def update_shape(self):
+
+        self.offsetx = self.width // 2
+        self.offsety = self.height // 2
+
+        if self.height < self.width:
+            radius = self.height // 2
+        else:
+            radius = self.width // 2
+
+        new_shape = pymunk.shapes.Circle(body=self.body, radius=radius)
+        new_shape.mass = self.mass
+        new_shape.color = (255, 0, 0, 100)
+
+        if hasattr(self, 'shape') and self.shape in self.space.bodies:
+            self.space.remove(self.shape)
+            print("Removed Shape")
+
+        self.shape = new_shape
+        self.add_to_space()
+
+    def change_scale(self, scale):
+        print("Resizing to " + str(scale))
+        self.scale = scale
+        self.update_shape()
+
 
 
 # Class to track player's ship and stats.
@@ -41,9 +72,9 @@ class PlayerShip(GameObject):
 
         super().__init__(*args, **kwargs)
 
-        self.shape = pymunk.shapes.Circle(body=self.body,radius=100)
-        self.shape.mass = 10
+        # TODO - Make better shapes for collision detectio
         self.shape.fricton = 1  # This only affects collisions, not general movement.
+        self.shape.color = (0, 255, 0, 100)
 
         if 'img' in kwargs:
             self.img = kwargs['img']
--- SpacePotato.py
+++ SpacePotato.py
@@ -3,8 +3,10 @@ import pymunk
 from InputHandler import PlayerControl
 from GameObjects import PlayerShip
 global GameWindow # TODO - Get rid of this horrible global variable. It may not be needed at all anyway.
+import pymunk.pyglet_util
 
-
+# Turn on debug shape rendering for pymunk
+DEBUG = True
 
 
 # Class to track our game window and environment
@@ -13,16 +15,26 @@ class GameWindow(pyglet.window.Window):
     UpdateList = []
 
     def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.foreground_batch = pyglet.graphics.Batch()
         self.background_batch = pyglet.graphics.Batch()
+        self.debug_batch = pyglet.graphics.Batch()
         self.player_control = PlayerControl(self)
         self.space = pymunk.Space()
         self.space.damping = 0.1    # Effectively drag/friction for space... stupid but makes control feel nicer.
-        super().__init__(**kwargs)
+
+        if DEBUG:
+            self.debug_draw_options = pymunk.pyglet_util.DrawOptions()
+            self.debug_draw_options.flags = pymunk.SpaceDebugDrawOptions.DRAW_SHAPES
+            self.space.debug_draw(self.debug_draw_options)
+
+
 
     def on_draw(self):
         self.clear()
         self.background_batch.draw()
+        if DEBUG:
+            self.space.debug_draw(self.debug_draw_options)
         self.foreground_batch.draw()
 
     def register_for_update(self, game_object):
@@ -51,10 +63,11 @@ def init():
     pyglet.resource.reindex()
 
     # Create Player Object.
-    window.player = PlayerShip(img=pyglet.resource.image("potato.png"))
+    window.player = PlayerShip(img=pyglet.resource.image("potato.png"), space=window.space)
+    window.player.change_scale(0.2)
+    window.player.change_scale(0.2)
     window.player.batch = window.foreground_batch
     window.register_for_update(window.player)
-    window.player.add_to_space(window.space)
 
     # Kick off the Game's control loop.
     pyglet.clock.schedule_interval(window.master_update, 1 / 120.0)
